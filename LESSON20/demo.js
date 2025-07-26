3342d0e0d9f2e0514dbe4f573eef1f0445655b1b
minatodesuog7 <bhupendrarao91@gmail.com>
2025-07-26 21:13:02 +0530

    yoyoyoyoyo

---
+++ LESSON20/demo.js
@@ -0,0 +1,23 @@
+function muhahahhaahaa() {
+    console.log("🎵🎶IT's raining tacos🎵🎶")
+console.log("🎵🎶outta the sky tacos , no need to ask why🎵🎶")
+ console.log("✨TACOS✨")
+}
+
+muhahahhaahaa()
+muhahahhaahaa()
+muhahahhaahaa()
+
+function sum9(a, b, c, d, e) {
+    var sum = a + b + c + d + e
+    return result
+}
+
+var a = - sum(800, 564654, 5467, 5676575, 213324)
+console.log(a)
+
+function multiply(a,b,c,d,e) {
+    return a * b * c * d * e
+}
+
+console.log(multiply(1, 2, 3, 4, 5))
