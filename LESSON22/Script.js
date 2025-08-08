9cbcbb38c196497c8f8a6db7af90e7e5836968c2
minatodesuog7 <bhupendrarao91@gmail.com>
2025-08-08 21:26:08 +0530

    do quick

---
+++ LESSON22/Script.js
@@ -0,0 +1,12 @@
+var yo = {name: 'whywoulditellu' , Age: 'none of your concern' , city: 'i know you are hitman' , Country: 'Kalasis 54 ' , noneveg : true , printDetails: function(){
+console.log('i do care , just gimme burger')
+return 5
+}}
+
+console.log(yo.city)
+console.log(yo['city'])
+
+console.log(yo.printDetails())
+
+console.log(Math.random())
+console.log(Math.floor(8,7))
