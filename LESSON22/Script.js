6086d290912e8879d9c9ee9c65ca28edd8ffae79
minatodesuog7 <bhupendrarao91@gmail.com>
2025-08-15 20:57:03 +0530

    sorry

--- LESSON22/Script.js
+++ LESSON22/Script.js
@@ -1,3 +1,4 @@
+
 var yo = {name: 'whywoulditellu' , Age: 'none of your concern' , city: 'i know you are hitman' , Country: 'Kalasis 54 ' , noneveg : true , printDetails: function(){
 console.log('i do care , just gimme burger')
 return 5
---
+++ LESSON22/project.js
@@ -0,0 +1,14 @@
+// BY MISTAKE I HAVE SUBMITTED MY CODE ON LESSON23 BUT I HAAVE TO SUBMIT LESSO23
+// ACTUALLY I CREATED MY PROJECT DURING CLASS SO MY CLASS WORK IS MY PROJECT SORRY FOR THIS AND HERE IT IS 
+var yo = {name: 'whywoulditellu' , Age: 'none of your concern' , city: 'i know you are hitman' , Country: 'Kalasis 54 ' , noneveg : true , printDetails: function(){
+console.log('i do care , just gimme burger')
+return 5
+}}
+
+console.log(yo.city)
+console.log(yo['name'])
+
+console.log(yo.printDetails())
+
+console.log(Math.random())
+console.log(Math.floor(8,7))
