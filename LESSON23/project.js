6febaef6d02985a33acab00179f151f1de69c632
minatodesuog7 <bhupendrarao91@gmail.com>
2025-08-15 20:50:28 +0530

    yahoooo

---
+++ LESSON23/project.js
@@ -0,0 +1,17 @@
+var name = ['NARUTO' , 'SASUKE' , 'ITACHI' , 'IROCOSYLIS' , 'ABRA KA DAVARA' , 3466754 , false ] 
+   var a =  ['ambani' , 'dora the BATMAN' , 'KYA LIKHU']
+    console.log(name[4])
+    console.log(name[2+3])
+    console.log(a[1])
+    
+    function sum(a,b,c){
+        return a+b+c
+    }
+
+    function average(a,b,c){
+        return sum(a,b,c)/3
+    }
+
+    var result = average(4675678,56787686787,57685768)
+
+    console.log(result)
