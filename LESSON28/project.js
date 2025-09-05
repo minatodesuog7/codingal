b3c403fee5baf1ff137c894c658702daea2cf033
minatodesuog7 <bhupendrarao91@gmail.com>
2025-09-05 20:49:32 +0530

    projoooct

---
+++ LESSON28/project.js
@@ -0,0 +1,33 @@
+try{
+    const w = 100
+    w = 25
+}catch(err){
+    console.log(err)
+    console.log(' be brave soilders , a single code cant decide our future muhahaha we will still rise' )
+}
+var a = 'NARUTOOOO'
+var b = 76578
+var c = true 
+var x = false
+var d = null
+
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+console.log(typeof d)
+
+b = String(b)
+console.log(typeof b)
+
+c = Number(c)
+x = Number(x)
+console.log(typeof c)
+console.log(typeof x)
+
+function hailhitler(name){
+    console.log("we need comeback of " + name)
+}
+hailhitler("walid")
+
+var iwannarunaway = name => console.log("iwanna sing " + name)
+iwannarunaway("i am lonely")
