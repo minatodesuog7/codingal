d252105c29c08b101c4933c442e327fa5e5c6bb1
minatodesuog7 <bhupendrarao91@gmail.com>
2025-09-05 21:45:04 +0530

    go

---
+++ LESSON30/domo.js
@@ -0,0 +1,10 @@
+function walidissuperhero(){
+    var myform = document.forms['walidkaform']
+    if(myform['address'].value == 'abc'){
+        alert("if u come back, hitler will bomb ur hommie and lary will be there too")
+return
+    }
+    if(myform['phonenumber'].value == '123'){
+        alert("if u come back, hitler will bomb ur hommie and lary will be there too")
+return
+}}
