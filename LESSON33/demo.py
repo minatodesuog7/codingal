d0bdd43aff8afae1d29a3158750543d0493f82ef
minatodesuog7 <bhupendrarao91@gmail.com>
2025-11-01 21:07:27 +0530

    yj

---
+++ LESSON33/demo.py
@@ -0,0 +1,41 @@
+print("muhahahaha")
+name='idc'
+print(name)
+
+a = 546
+b = 756.758
+c = a + b
+d = a * b
+e = True
+f = False
+g = None
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(e))
+print(type(f))
+print(type(g))
+
+h = a + b
+i = a - b
+j = a / b
+k = a * b
+l = a // b
+n = a % b
+
+
+print(h)
+print(i)
+print(j)
+print(k)
+print(l)
+print(n)
+
+# loops in python
+for i in range(5):
+    print(i)
+
+while a < 600:
+    print(a)
+    a +=1    
