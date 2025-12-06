0b42f4703aba4d09f6b90b19d42627453059e797
minatodesuog7 <bhupendrarao91@gmail.com>
2025-12-06 21:11:03 +0530

    set

---
+++ LESSONIDK3/demo.py
@@ -0,0 +1,17 @@
+
+s1 = {1,2,3}
+s2 = {3,4,5}
+print(s1)
+print(s2)
+
+#union
+print(s1.union(s2))
+
+#intersection
+print(s1.intersection(s2))
+
+#difference
+print(s1.difference(s2))
+
+#symmetric difference
+print(s1.symmetric_difference(s2))
