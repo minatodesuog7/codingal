5ef83b2844cd8ea110c274d9346b8004e2b27af0
minatodesuog7 <bhupendrarao91@gmail.com>
2025-12-26 21:00:26 +0530

    yo

---
+++ LESSONIDK5/dem.py
@@ -0,0 +1,20 @@
+class person:
+
+    def __init__(self, name, id_number):
+        self.name = name
+        self.id_number = id_number
+
+    def display(self):
+
+        print(self.name)
+        print(self.id_number)
+class employee(person):
+    def __init__(self, name, id_number, salary, post):
+        self.salary=salary
+        self.post=post
+
+        person.__init__(self, name, id_number)
+obj = employee("penguin", 20210401401, 15000, "intern")
+obj.display()
+print(obj.salary)
+print(obj.post)
